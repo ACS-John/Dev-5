698e62c98b661cf90e28dd7265fcd26c542dc069
niceguywinning <niceguywinning@a857be20-dd21-49df-aee6-eedf40c70b90>
2019-07-04 16:33:35 +0000

    upgrade br to 4.32j (from 4.32k)

--- Core/Client/ACS 5 - Client.cmd
+++ Core/Client/ACS 5 - Client.cmd
@@ -3,7 +3,8 @@
 @Echo Launching Client...
 
 rem Set BR_Date=2017-04-19
-Set BR_Date=2018-07-28
+rem Set BR_Date=2018-07-28
+Set BR_Date=2019-05-31
 
 Rem If Exist %SystemRoot%\SysWow64 (
 Rem     Set BR_Architecture=x64
@@ -11,12 +12,12 @@ Rem ) else (
     Set BR_Architecture=Win32
 Rem )
 
-If /I "%ACSDeveloper%" NEQ "" (
-    @Echo      ACSDeveloper:  Yes (%ACSDeveloper%)
-    Set BR_Model=DebugEfence
-) else (
+Rem If /I "%ACSDeveloper%" NEQ "" (
+Rem     @Echo      ACSDeveloper:  Yes (%ACSDeveloper%)
+Rem     Set BR_Model=DebugEfence
+Rem ) else (
     Set BR_Model=Release
-)
+Rem )
 @Echo              Mode:          %BR_Model%
 
 Set BR=%~dp0brclient-%BR_Architecture%-%BR_Model%-%BR_Date%.exe
